808eeb2c4c9c24704007541f9762bb636e39efbb
LeeHungry <liwenqing2008@126.com>
2017-11-18 11:20:01 +0800

    Initial commit

---
+++ utils/src/main/java/deepin/com/utils/AppUtils.java
@@ -0,0 +1,193 @@
+package deepin.com.utils;
+
+import android.app.ActivityManager;
+import android.content.ComponentName;
+import android.content.Context;
+import android.content.pm.ApplicationInfo;
+import android.content.pm.PackageInfo;
+import android.content.pm.PackageManager;
+import android.graphics.drawable.Drawable;
+
+import java.util.List;
+
+import javax.security.auth.x500.X500Principal;
+
+/**
+ * Created by Li on 2017/11/18.
+ */
+
+public class AppUtils {
+    private final static X500Principal DEBUG_DN = new X500Principal("CN=Android Debug,O=Android,C=US");
+
+    /**
+     * Judge whether an app is in background
+     */
+    public static boolean isAppBackground(Context context) {
+        ActivityManager am = (ActivityManager) context.getSystemService(Context.ACTIVITY_SERVICE);
+        List<ActivityManager.RunningTaskInfo> taskList = am.getRunningTasks(1);
+        if (taskList != null && !taskList.isEmpty()) {
+            ComponentName topActivity = taskList.get(0).topActivity;
+            if (topActivity != null
+                    && !topActivity.getPackageName().equals(context.getPackageName())) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    /**
+     * Get version name
+     */
+    public static String getVersionName(Context context) {
+        PackageInfo info;
+        try {
+            info = context.getPackageManager().getPackageInfo(context.getPackageName(), 0);
+            return info.versionName;
+        } catch (PackageManager.NameNotFoundException e) {
+            // e.printStackTrace();
+        }
+        return "";
+    }
+
+    /**
+     * Get version code
+     */
+    public static int getVersionCode(Context context) {
+        PackageInfo info;
+        try {
+            info = context.getPackageManager().getPackageInfo(context.getPackageName(), 0);
+            return info.versionCode;
+        } catch (PackageManager.NameNotFoundException e) {
+            // e.printStackTrace();
+        }
+        return 0;
+    }
+
+    /**
+     * Get package name
+     */
+    public static String getPackageName(Context context) {
+        return context.getPackageName();
+    }
+
+    /**
+     * Get icon
+     */
+    public static Drawable getIcon(Context context) {
+        return getAppIcon(context, getPackageName(context));
+    }
+
+    /**
+     * Get app icon
+     */
+    public static Drawable getAppIcon(Context context, String packageName) {
+        try {
+            PackageManager pm = context.getPackageManager();
+            ApplicationInfo info = pm.getApplicationInfo(packageName, 0);
+            return info.loadIcon(pm);
+        } catch (PackageManager.NameNotFoundException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+    /**
+     * Get app version name
+     */
+    public static String getAppVersionName(Context context, String packageName) {
+        try {
+            PackageManager pm = context.getPackageManager();
+            PackageInfo packageInfo = pm.getPackageInfo(packageName, 0);
+            return packageInfo.versionName;
+        } catch (PackageManager.NameNotFoundException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+    /**
+     * Get app version code
+     */
+    public static int getAppVersionCode(Context context, String packageName) {
+        try {
+            PackageManager pm = context.getPackageManager();
+            PackageInfo packageInfo = pm.getPackageInfo(packageName, 0);
+            return packageInfo.versionCode;
+        } catch (PackageManager.NameNotFoundException e) {
+            e.printStackTrace();
+        }
+        return -1;
+    }
+
+    /**
+     * Get app name
+     */
+    public static String getAppName(Context context, String packageName) {
+        try {
+            PackageManager pm = context.getPackageManager();
+            ApplicationInfo info = pm.getApplicationInfo(packageName, 0);
+            return info.loadLabel(pm).toString();
+        } catch (PackageManager.NameNotFoundException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+    /**
+     * Get app permission
+     */
+    public static String[] getAppPermission(Context context, String packageName) {
+        try {
+            PackageManager pm = context.getPackageManager();
+            PackageInfo packageInfo = pm.getPackageInfo(packageName, PackageManager.GET_PERMISSIONS);
+            return packageInfo.requestedPermissions;
+        } catch (PackageManager.NameNotFoundException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+    /**
+     * Get app signature
+     */
+    public static String getAppSignature(Context context, String packageName) {
+        try {
+            PackageManager pm = context.getPackageManager();
+            PackageInfo packageInfo = pm.getPackageInfo(packageName, PackageManager.GET_SIGNATURES);
+            return packageInfo.signatures[0].toCharsString();
+        } catch (PackageManager.NameNotFoundException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+    /**
+     * Judge whether an app is dubuggable
+     */
+    public static boolean isApkDebuggable(Context context) {
+        try {
+            ApplicationInfo info = context.getApplicationInfo();
+            return (info.flags & ApplicationInfo.FLAG_DEBUGGABLE) != 0;
+        } catch (Exception e) {
+
+        }
+        return false;
+    }
+
+    /**
+     * Judge whether an app is dubuggable by package name
+     */
+    public static boolean isApkDebugable(Context context, String packageName) {
+        try {
+            PackageInfo pkginfo = context.getPackageManager().getPackageInfo(
+                    packageName, PackageManager.GET_ACTIVITIES);
+            if (pkginfo != null) {
+                ApplicationInfo info = pkginfo.applicationInfo;
+                return (info.flags & ApplicationInfo.FLAG_DEBUGGABLE) != 0;
+            }
+        } catch (Exception e) {
+        }
+        return false;
+    }
+
+}
